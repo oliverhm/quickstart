27edee334c558fcdf64628bd487e658c85ab0e78
Oliver Henrique Maceira <oliver@ciandt.com>
2017-09-18 18:06:27 -0300

    Altera o array de Heróis

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,16 @@
 import {Component} from '@angular/core';
 
 const HEROES: Array<Hero> = [
-  { id: 11, name: "Mr. Nice" },
-  { id: 12, name: "Oliver Queen" },
-  { id: 13, name: "Lucas Biba" },
-  { id: 14, name: "Guilherme Chefao" },
-  { id: 15, name: "Bombasto" },
-  { id: 16, name: "Celeristas" },
-  { id: 17, name: "Magneta" },
-  { id: 18, name: "RubberMan" },
-  { id: 19, name: "Dynama" },
-  { id: 20, name: "Tornado" }
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
 ];
 
 @Component({
