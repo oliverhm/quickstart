fab4fec0b27864db6fcb4558a440798a807a1b6b
Guilherme Oliveira <gmoliveira@ciandt.com>
2017-09-18 17:16:26 -0300

    Treinamento angular

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,16 +1,23 @@
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { By }           from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {By} from '@angular/platform-browser';
+import {DebugElement} from '@angular/core';
+import {FormsModule} from "@angular/forms";
 
 describe('AppComponent', function () {
-  let de: DebugElement;
   let comp: AppComponent;
   let fixture: ComponentFixture<AppComponent>;
+  let elh1: DebugElement;
+  let elh2: DebugElement;
+  let elDivHeroId: DebugElement;
+  let elDivHeroName: DebugElement;
+  let elInput: DebugElement;
+  let elButtonHero: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [ FormsModule ],
       declarations: [ AppComponent ]
     })
     .compileComponents();
@@ -19,15 +26,36 @@ describe('AppComponent', function () {
   beforeEach(() => {
     fixture = TestBed.createComponent(AppComponent);
     comp = fixture.componentInstance;
-    de = fixture.debugElement.query(By.css('h1'));
+    elh1 = fixture.debugElement.query(By.css('h1'));
+    elh2 = fixture.debugElement.query(By.css('h2.hero-details'));
+    elDivHeroId = fixture.debugElement.query(By.css('div#heroId'));
+    elDivHeroName = fixture.debugElement.query(By.css('div#heroName'));
+    elInput = fixture.debugElement.query(By.css('input'));
+    elButtonHero = fixture.debugElement.query(By.css('.heroes'));
   });
 
   it('should create component', () => expect(comp).toBeDefined() );
 
   it('should have expected <h1> text', () => {
     fixture.detectChanges();
-    const h1 = de.nativeElement;
-    expect(h1.innerText).toMatch(/angular/i,
-      '<h1> should say something about "Angular"');
+    const h1 = elh1.nativeElement;
+    expect(h1.innerText).toEqual("Tour of Heroes");
+  });
+
+  it('should be show an Hero', (done: DoneFn) => {
+    fixture.detectChanges();
+    const button = elButtonHero.children;
+
+    console.log('button', button[1]);
+
+    button[0].triggerEventHandler('click', null);
+    fixture.detectChanges();
+    setTimeout(() => {
+      const h2 = elh2.nativeElement;
+      console.log('h2.innerText', h2.innerText);
+      expect(h2.innerText).toEqual("Lucas Biba");
+      done();
+    }, 500);
   });
 });
+
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,49 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+
+
+const HEROES: Hero[] = [
+  {id: 11, name: "Mr. Nice"},
+  {id: 12, name: "Oliver Queen"},
+  {id: 13, name: "Lucas Biba"},
+  {id: 14, name: "Guilherme Chefao"},
+  {id: 15, name: "Bombasto"},
+  {id: 16, name: "Celeristas"},
+  {id: 17, name: "Magneta"},
+  {id: 18, name: "RubberMan"},
+  {id: 19, name: "Dynama"},
+  {id: 20, name: "Tornado"}
+];
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `
+    <h1>{{title}}</h1>
+    <h2>My Heroes</h2>
+    <ul class="heroes">
+      <li class="hero-item" *ngFor="let hero of heroes" (click)="onSelect(hero)">
+        <span class="badge">{{hero.id}}</span> {{hero.name}}
+      </li>
+    </ul>
+    <div *ngIf="selectedHero">
+    <h2 class="hero-details">{{selectedHero.name}} details!</h2>
+    <div id="heroId"><label>id: </label>{{selectedHero.id}}</div>
+    <div id="heroName">
+      <label>name: </label>
+      <input [(ngModel)]="selectedHero.name" placeholder="name">
+    </div></div>
+  `,
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent {
+  title: string = "Tour of Heroes";
+  selectedHero: Hero;
+  heroes = HEROES;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+}
+
+export class Hero {
+  id: number;
+  name: string;
+}
